2482a22a561ff569f862e1c5c89df04f36c77453
camello3131 <lucasbritosboccazzi@outlook.com>
2023-05-18 08:23:38 -0300

    asd

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -2,7 +2,6 @@ import React, {useState, useEffect} from 'react'
 import {useParams} from "react-router-dom"
 import {getFirestore, doc, getDocs, collection } from "firebase/firestore"
 import { app } from '../../firebase/config'
-import ItemDetail from '../ItemDetail/ItemDetail.jsx'
 
 export const ItemDetailContainer = () => {
   const [item, setItem] = useState({});
@@ -34,7 +33,6 @@ export const ItemDetailContainer = () => {
         </div>
       ) : (
         <div>
-          <ItemDetail item={item} ></ItemDetail>
         </div>
       )}
     </div>
